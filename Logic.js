0da4078dd23ab7099e79763e1789785b4db251d2
Mike <mvmalfara@gmail.com>
2022-12-14 21:16:04 -0500

    third commit

---
+++ Logic.js
@@ -0,0 +1,22 @@
+//Highlights clicked squares
+function stuff() {
+    unhighlight()
+    this.style.backgroundColor = 'grey'
+
+    id = this
+}
+
+
+
+// the highlight function will take an element as an argument
+function unhighlight() {
+    id.style.backgroundColor = 'black'
+}
+
+function CheckLegalMove() {
+   
+}
+
+function Move() {
+   
+}
--- main.js
+++ main.js
@@ -85,58 +85,126 @@ board.placePiece(blackpiece12, 7, 7);
 
 //sets id as a variable to temporarily reference "a1" resolving backgroundColor reference errors.
 //This variable is used to hold previously clicked element
-var id = document.getElementById("a1")
+let id = document.getElementById("a1")
 
-const RedPiece = new Image(50, 50);
-const BlackPiece = new Image(50, 50);
-RedPiece.src = "https://cdn0.iconfinder.com/data/icons/board-games-flat-1/48/Games_BoardGames_Artboard_14-512.png";
-BlackPiece.src = "https://cdn0.iconfinder.com/data/icons/board-games-colored-1/48/Games_BoardGames_Artboard_15-512.png";
 
+//create ids for each place on the board
+let a1 = document.getElementById("a1")
+let a3 = document.getElementById("a3")
+let a5 = document.getElementById("a5")
+let a7 = document.getElementById("a7")
+
+let b2 = document.getElementById("b2")
+let b4 = document.getElementById("b4")
+let b6 = document.getElementById("b6")
+let b8 = document.getElementById("b8")
+
+let c1 = document.getElementById("c1")
+let c3 = document.getElementById("c3")
+let c5 = document.getElementById("c5")
+let c7 = document.getElementById("c7")
+
+let d2 = document.getElementById("d2")
+let d4 = document.getElementById("d4")
+let d6 = document.getElementById("d6")
+let d8 = document.getElementById("d8")
+
+let e1 = document.getElementById("e1")
+let e3 = document.getElementById("e3")
+let e5 = document.getElementById("e5")
+let e7 = document.getElementById("e7")
+
+let f2 = document.getElementById("f2")
+let f4 = document.getElementById("f4")
+let f6 = document.getElementById("f6")
+let f8 = document.getElementById("f8")
+
+let g1 = document.getElementById("g1")
+let g3 = document.getElementById("g3")
+let g5 = document.getElementById("g5")
+let g7 = document.getElementById("g7")
+
+let h2 = document.getElementById("h2")
+let h4 = document.getElementById("h4")
+let h6 = document.getElementById("h6")
+let h8 = document.getElementById("h8")
+
+//add red pieces to the red side
+a1.classList.add("red")
+a3.classList.add("red")
+a5.classList.add("red")
+a7.classList.add("red")
+
+b2.classList.add("red")
+b4.classList.add("red")
+b6.classList.add("red")
+b8.classList.add("red")
+
+c1.classList.add("red")
+c3.classList.add("red")
+c5.classList.add("red")
+c7.classList.add("red")
+
+
+//add black pieces to the black side
+f2.classList.add("black")
+f4.classList.add("black")
+f6.classList.add("black")
+f8.classList.add("black")
+
+g1.classList.add("black")
+g3.classList.add("black")
+g5.classList.add("black")
+g7.classList.add("black")
+
+h2.classList.add("black")
+h4.classList.add("black")
+h6.classList.add("black")
+h8.classList.add("black")
 
 
 //adds click listeners to squares in HTML
-document.getElementById("a1").addEventListener("click", stuff);
-document.getElementById("a3").addEventListener("click", stuff);
-document.getElementById("a5").addEventListener("click", stuff);
-document.getElementById("a7").addEventListener("click", stuff);
+a1.addEventListener("click", stuff);
+a3.addEventListener("click", stuff);
+a5.addEventListener("click", stuff);
+a7.addEventListener("click", stuff);
 
-document.getElementById("b2").addEventListener("click", stuff);
-document.getElementById("b4").addEventListener("click", stuff);
-document.getElementById("b6").addEventListener("click", stuff);
-document.getElementById("b8").addEventListener("click", stuff);
+b2.addEventListener("click", stuff);
+b4.addEventListener("click", stuff);
+b6.addEventListener("click", stuff);
+b8.addEventListener("click", stuff);
 
-document.getElementById("c1").addEventListener("click", stuff);
-document.getElementById("c3").addEventListener("click", stuff);
-document.getElementById("c5").addEventListener("click", stuff);
-document.getElementById("c7").addEventListener("click", stuff);
+c1.addEventListener("click", stuff);
+c3.addEventListener("click", stuff);
+c5.addEventListener("click", stuff);
+c7.addEventListener("click", stuff);
 
-document.getElementById("d2").addEventListener("click", stuff);
-document.getElementById("d4").addEventListener("click", stuff);
-document.getElementById("d6").addEventListener("click", stuff);
-document.getElementById("d8").addEventListener("click", stuff);
+d2.addEventListener("click", stuff);
+d4.addEventListener("click", stuff);
+d6.addEventListener("click", stuff);
+d8.addEventListener("click", stuff);
 
-document.getElementById("e1").addEventListener("click", stuff);
-document.getElementById("e3").addEventListener("click", stuff);
-document.getElementById("e5").addEventListener("click", stuff);
-document.getElementById("e7").addEventListener("click", stuff);
+e1.addEventListener("click", stuff);
+e3.addEventListener("click", stuff);
+e5.addEventListener("click", stuff);
+e7.addEventListener("click", stuff);
 
-document.getElementById("f2").addEventListener("click", stuff);
-document.getElementById("f4").addEventListener("click", stuff);
-document.getElementById("f6").addEventListener("click", stuff);
-document.getElementById("f8").addEventListener("click", stuff);
+f2.addEventListener("click", stuff);
+f4.addEventListener("click", stuff);
+f6.addEventListener("click", stuff);
+f8.addEventListener("click", stuff);
 
-document.getElementById("g1").addEventListener("click", stuff);
-document.getElementById("g3").addEventListener("click", stuff);
-document.getElementById("g5").addEventListener("click", stuff);
-document.getElementById("g7").addEventListener("click", stuff);
+g1.addEventListener("click", stuff);
+g3.addEventListener("click", stuff);
+g5.addEventListener("click", stuff);
+g7.addEventListener("click", stuff);
 
-document.getElementById("h2").addEventListener("click", stuff);
-document.getElementById("h4").addEventListener("click", stuff);
-document.getElementById("h6").addEventListener("click", stuff);
-document.getElementById("h8").addEventListener("click", stuff);
+h2.addEventListener("click", stuff);
+h4.addEventListener("click", stuff);
+h6.addEventListener("click", stuff);
+h8.addEventListener("click", stuff);
 
-document.getElementById("a1").appendChild(RedPiece);
-document.getElementById("h2").appendChild(BlackPiece);
+
 
 
 
@@ -144,7 +212,6 @@ document.getElementById("h2").appendChild(BlackPiece);
 function stuff() {
     unhighlight()
     this.style.backgroundColor = 'grey'
-
     id = this
 }
 
